9e5478739148dba0d462476d3e88edce2e499cb8
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-05-23 23:01:15 -0700

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        cur_gas = 0
+        index = 0
+        lowest = 0
+
+        if sum(gas) < sum(cost):
+            return -1
+
+        for i, pair in enumerate(zip(gas, cost)):
+            gas_add, gas_cost = pair
+            cur_gas += gas_add
+
+            if gas_cost > cur_gas:
+                index = i + 1
+                lowest = min(lowest, cur_gas - gas_cost)
+                cur_gas = 0
+            else:
+                cur_gas -= gas_cost
+
+        index %= len(gas)
+        
+        return index
+        
