c11729f4a276e2ac0d197bf59dced7e8e7b18747
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-06-11 20:54:33 -0700

    Add: count-squares - submission-2

---
+++ Data Structures & Algorithms/count-squares/submission-2.py
@@ -0,0 +1,38 @@
+class CountSquares:
+
+    def __init__(self):
+        self.points = {}
+        self.point_list = []
+        
+
+    def add(self, point: List[int]) -> None:
+        point = tuple(point)
+
+        if point in self.points:
+            self.points[point] += 1
+        else:
+            self.points[point] = 1
+
+        
+
+    def count(self, point: List[int]) -> int:
+        x, y = point
+        total = 0
+
+        for other_x, other_y in self.points:
+
+            add = self.points[(other_x, other_y)]
+            if x == other_x and y == other_y:
+                continue
+
+            if abs(x - other_x) == abs(y - other_y):
+                corner_one = (x, other_y)
+                corner_two = (other_x, y)
+                if corner_one in self.points and corner_two in self.points:
+                    total += add * self.points[corner_two] * self.points[corner_one]
+        return total
+
+
+            
+
+        
