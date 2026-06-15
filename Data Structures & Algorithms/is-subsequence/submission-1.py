615109da8400ec8f6554f692b2686d22ea4a99ab
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-06-15 02:38:01 -0700

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+
+        letter_pointer = 0
+
+        for l in t:
+            if l == s[letter_pointer]:
+                letter_pointer += 1
+            if letter_pointer == len(s):
+                return True
+        
+        return False
+        
