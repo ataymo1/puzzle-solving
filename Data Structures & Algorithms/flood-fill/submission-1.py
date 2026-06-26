2324a4a9d8aa6d6fb0d7ed44326891d69f6d8379
Atay Moya <112656287+ataymo1@users.noreply.github.com>
2026-06-26 04:15:58 -0700

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        toChange = image[sr][sc]
+
+        if color == toChange:
+            return image
+
+        queue = deque()
+        queue.append([(sr, sc)])
+
+        while queue:
+            cur_level = queue.popleft()
+            next_level = []
+            for row, col in cur_level:
+                image[row][col] = color
+                neighbors = [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]
+
+                for n in neighbors:
+                    n_row, n_col = n
+                    if n_row < 0 or n_row >= len(image) or n_col < 0 or n_col >= len(image[0]):
+                        continue
+                    if image[n_row][n_col] != toChange:
+                        continue
+
+                    next_level.append(n)
+                if next_level:
+                    queue.append(next_level)
+        
+        return image
+                    
+
+
+
+        
